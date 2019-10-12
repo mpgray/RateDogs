a0accd6491941ec96b741643a01ca1b0cd297e3e
Michael Gray <michael@mgray.me>
2019-10-12 01:01:59 -0600

    Clean up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {RateComponent} from './rate/rate.component';
-import {DashboardComponent} from './dashboard/dashboard.component';
-import {DogsComponent} from './dogs/dogs.component';
-
 
 const routes: Routes = [
   {
